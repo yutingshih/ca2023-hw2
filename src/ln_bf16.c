7e6e3a1e8b697790c966fa1a44c03c47b6a74b94
Ray <51779943+coding-ray@users.noreply.github.com>
2023-10-07 22:39:57 +0800

    feat: bf16 ln dataset generation in C v0.2

--- src/ln_bf16.c
+++ src/ln_bf16.c
@@ -2,8 +2,8 @@
  * This program implements and tests the following functionality:
  *   Natural logarithm of fp32 and bf16 numbers.
  *
- * Version: 0.1
- * Tested: 2023-10-07T22:33:00+08:00
+ * Version: 0.2
+ * Tested: 2023-10-07T22:39:00+08:00
  */
 
 #ifndef LN_BF16_C
@@ -16,12 +16,18 @@
 
 // uncomment the following line to test this program
 // #define LN_BF16_TEST
+
 #ifdef LN_BF16_TEST
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 
 #include "fp32_bf16.c"
 #include "print_bf16.c"
+
+// uncomment the following line to generate the dataset
+// #define LN_BF16_GENERATE_DATASET
+
 #endif  // LN_BF16_TEST
 
 /* ln(abs(x))
@@ -35,11 +41,11 @@
  * Reference: https://quadst.rip/ln-approx.html
  */
 float ln_fp32(float x) {
-  unsigned int *px = (unsigned int *)&x;
+  i32 *px = (i32 *)&x;
 
   // catch zero
   if (*px == 0) {
-    *px = 0xFF800000;
+    *px = 0xFF800000;  // -inf
     return *(bf16 *)px;
   }
 
@@ -47,7 +53,7 @@ float ln_fp32(float x) {
   *px &= 0x7FFFFFFF;
 
   // get exponent
-  int exp = (*px >> 23) - 127;
+  i32 exp = (*px >> 23) - 127;
 
   // set x's exponent to 0, which is 127 after normalization.
   *px = 0x3F800000 | (*px & 0x7FFFFF);
@@ -67,14 +73,14 @@ float ln_fp32(float x) {
  * This function only works in a 32-bit runtime.
  */
 bf16 ln_bf16(bf16 x) {
-  // Constants for bf16 in the precision of bf16
+  // constants for bf16 in the precision of bf16
   const u32 u_lnc0 = 0xBFBF0000;  // -1.49
   const u32 u_lnc1 = 0x40070000;  // 2.11
   const u32 u_lnc2 = 0xBF3B0000;  // -0.73
   const u32 u_lnc3 = 0x3DE10000;  // 0.109
   const u32 u_ln2 = 0x3F310000;   // 0.69
 
-  // Constants for this function in the precision of bf16
+  // constants for this function in the precision of bf16
   const bf16 lnc0 = *(bf16 *)&u_lnc0;  // -1.49
   const bf16 lnc1 = *(bf16 *)&u_lnc1;  // 2.11
   const bf16 lnc2 = *(bf16 *)&u_lnc2;  // -0.73
@@ -82,13 +88,16 @@ bf16 ln_bf16(bf16 x) {
   const bf16 ln2 = *(bf16 *)&u_ln2;    // 0.69
 
   u32 *px = (u32 *)&x;
-  bf16 exp = i32_to_bf16(((*px & 0x7F800000) >> 23) - 127);
+  // remove extra bits (otherwise, offset-by-one bug occurs)
+  *px = *px & 0x7FFF0000;
 
   // catch zero
   if (*px == 0) {
     *px = 0xFF800000;
     return *(bf16 *)px;
   }
+
+  bf16 exp = i32_to_bf16(((*px & 0x7F800000) >> 23) - 127);
 
   // set x's exponent to 0, which is 127 after normalization.
   *px = 0x3F800000 | (*px & 0x7F0000);
@@ -103,56 +112,85 @@ bf16 ln_bf16(bf16 x) {
 }
 
 #ifdef LN_BF16_TEST
-void print_comparison_fp32_bf16(float f, bf16 b) {
-  printf("%5.2f", fp32_to_bf16(f));
-  print_bf16_binary(fp32_to_bf16(f));
-  printf("%5.2f", b);
-  print_bf16_binary(b);
-  puts("");
+
+#ifdef LN_BF16_GENERATE_DATASET
+void print_fp32_bf16_comparison_header() {
+  puts(
+      "   x, x (bf16, hex),  ln(x), ln_fp32(x), ln_bf16(x), ln_bf16(x) (hex), "
+      "  error");
+}
+
+void print_fp32_bf16_comparison_row(float x, float t, float f, bf16 b) {
+  bf16 bf_in = fp32_to_bf16(x);
+  printf("%4.2f, %5s%08X, %6.3f, ", x, "0x", *(u32 *)&bf_in, t);
+  printf("%10.3f, %10.3f, %8s%08X, ", f, b, "0x", *(u32 *)&b);
+  printf("%7.3f\n", t - b);
+}
+#endif  // LN_BF16_GENERATE_DATASET
+
+/*
+ * if x = 0, return 0
+ * if 0 < x <= 9, return 10
+ * if 10 < x <= 99, return 100
+ * ...
+ *
+ * Note: a u32 number is less than or equal to 4,294,967,295.
+ */
+u32 get_smallest_decimal_number(u32 x) {
+  const u32 one_billion = 1000000000;
+  if (x > one_billion) return one_billion;
+  if (x == 0) return 0;
+  u32 divisor = 10;
+  while (x / divisor != 0) divisor *= 10;
+  return divisor;
 }
 
 /* Test the functionalities in this unit.
  * Return 0 if successes. Otherwise, return a non-zero number,
  * which indicates the first failed test.
  */
-int test_ln_bf16() {
-  float f, rf;
-  bf16 b, rb;
+int test_ln_bf16(float n_rows, float *average_error, float *maximal_error) {
+#ifdef LN_BF16_GENERATE_DATASET
+  print_fp32_bf16_comparison_header();
+#endif  // LN_BF16_GENERATE_DATASET
 
-  // 1: ln(1) = 0
-  f = b = i32_to_bf16(1);
-  rf = ln_fp32(f);
-  rb = ln_bf16(b);
-  printf("Ln test 1: ln(1) = 0\n");
-  print_comparison_fp32_bf16(rf, rb);
+  n_rows = roundf(n_rows);
+  *average_error = 0;
+  *maximal_error = 0;
+  float error_code_multiplier = (float)get_smallest_decimal_number((u32)n_rows);
+  float step = 2.0 / n_rows;
+  for (float f = 0; f <= 2.0001; f += step) {
+    float t = logf(f);
+    bf16 rb = ln_bf16(fp32_to_bf16(f));
+    bf16 error = fabsf(t - rb);
+    if (isfinite(error)) {
+      *average_error += error / n_rows;
+      if (error > 0.05)  // something is wrong.
+        return (int)(f * error_code_multiplier);
+      if (error > *maximal_error) *maximal_error = error;
+    }
 
-  // 2: ln(2) = 0.693
-  f = b = fp32_to_bf16(2.0f);
-  rf = ln_fp32(f);
-  rb = ln_bf16(b);
-  printf("Ln test 2: ln(2) = 0.693\n");
-  print_comparison_fp32_bf16(rf, rb);
-
-  // 3: ln(0.5) = -0.693
-  f = b = fp32_to_bf16(0.5f);
-  rf = ln_fp32(f);
-  rb = ln_bf16(b);
-  printf("Ln test 3: ln(0.5) = -0.693\n");
-  print_comparison_fp32_bf16(rf, rb);
-
-  // 4: ln(0.1) = -2.302
-  f = b = fp32_to_bf16(0.1f);
-  rf = ln_fp32(f);
-  rb = ln_bf16(b);
-  printf("Ln test 4: ln(0.1) = -2.302\n");
-  print_comparison_fp32_bf16(rf, rb);
+#ifdef LN_BF16_GENERATE_DATASET
+    float rf = ln_fp32(f);
+    print_fp32_bf16_comparison_row(f, t, rf, rb);
+#endif  // LN_BF16_GENERATE_DATASET
+  }
 
   return 0;
 }
 
 int main() {
-  test_ln_bf16();
-  return 0;
+  float average_error = 0, maximal_error = 0;
+  int error_code = test_ln_bf16(40, &average_error, &maximal_error);
+  if (error_code == 0) {
+    puts("Test for ln_bf16.c passed.");
+    printf("Average error: %.3f\n", average_error);
+    printf("Maximal error: %.3f\n", maximal_error);
+    return 0;
+  } else {
+    printf("Test %.2f for ln_bf16.c failed.\n", error_code / 100.0);
+    return 1;
+  }
 }
 #endif  // LN_BF16_TEST
 
